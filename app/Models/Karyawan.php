8b862fd2686b7e2569b7e97a3d7deb8d1657530a
HanifAjiPrasetyo <hanifaji007@gmail.com>
2024-01-03 16:56:33 +0700

    set up database, model, & relation

---
+++ app/Models/Karyawan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Karyawan extends Model
+{
+    use HasFactory;
+    protected $table = 'karyawans';
+    protected $fillable = ['nomor_induk', 'nama', 'tgl_lahir', 'alamat', 'tgl_lahir', 'tgl_gabung'];
+
+    public function cuti()
+    {
+        return $this->hasMany(Cuti::class, 'nomor_induk', 'nomor_induk');
+    }
+}
